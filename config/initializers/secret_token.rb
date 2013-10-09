75512b1cc0293bc4c68e76f5335c6aa7fe6fbd61
Gabi Nagy <gabrian.nagy@gmail.com>
2013-10-09 10:14:45 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reciclopedia::Application.config.secret_token = 'f21650b83b327db4520c7d17bb5b29d234a81a18982c90bdebc3e3d90622de052a8b4d4f78af69eda894a92563438b35912eeb9f256e22d583848b87e65d908a'
