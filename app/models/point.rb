c1c1b155f66ea6530322f66e04a513b3a9071a95
Gabi Nagy <gabrian.nagy@gmail.com>
2013-10-13 22:59:26 +0300

    Added user_id to point

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,2 +1,5 @@
 class Point < ActiveRecord::Base
+
+  belongs_to :user
+
 end
---
+++ db/migrate/20131013195253_add_user_id_to_point.rb
@@ -0,0 +1,5 @@
+class AddUserIdToPoint < ActiveRecord::Migration
+  def change
+    add_column :points, :user_id, :integer
+  end
+end
